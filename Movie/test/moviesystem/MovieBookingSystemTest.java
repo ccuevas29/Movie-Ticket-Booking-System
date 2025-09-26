44c077056d03b3c0c4ae21ed18b7db48f6510ad8
ccuevas29 <cherlize.cuevas@awsys-i.com>
2025-09-26 14:13:25 +0800

    Update MovieBookingSystemTest.java

--- Movie/test/moviesystem/MovieBookingSystemTest.java
+++ Movie/test/moviesystem/MovieBookingSystemTest.java
@@ -1,6 +1,8 @@
 package moviesystem;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
