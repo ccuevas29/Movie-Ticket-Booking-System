e6f7b79a282b6d4742aa217c2e8208b2fd3f05d2
ccuevas29 <cherlize.cuevas@awsys-i.com>
2025-09-26 13:32:56 +0800

    Update MovieBookingSystemTest.java

--- Movie/test/moviesystem/MovieBookingSystemTest.java
+++ Movie/test/moviesystem/MovieBookingSystemTest.java
@@ -5,19 +5,43 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MovieBookingSystemTest {
-    private MovieBookingSystem mbs;
+    /**
+      * Instance of the MovieBookingSystem under test.
+      */
+     private MovieBookingSystem mbs;
 
-    // Constants to avoid magic numbers
-    private static final String SHOW_10_AM = "10:00 AM";
-    private static final String SHOW_1_PM = "1:00 PM";
-    private static final String SHOW_9_PM = "9:00 PM";
-    private static final int TICKETS_50 = 50;
-    private static final int TICKETS_20 = 20;
-    private static final int BOOK_5 = 5;
-    private static final int BOOK_2 = 2;
-    private static final int CANCEL_3 = 3;
-    private static final int CANCEL_5 = 5;
-    private static final int OVERBOOK = 100;
+     // Constants to avoid magic numbers
+
+     /** Show time at 10:00 AM. */
+     private static final String SHOW_10_AM = "10:00 AM";
+
+     /** Show time at 1:00 PM. */
+     private static final String SHOW_1_PM = "1:00 PM";
+
+     /** Show time at 9:00 PM. */
+     private static final String SHOW_9_PM = "9:00 PM";
+
+     /** Ticket capacity of 50 for a show. */
+     private static final int TICKETS_50 = 50;
+
+     /** Ticket capacity of 20 for a show. */
+     private static final int TICKETS_20 = 20;
+
+     /** Number of 5 tickets to book. */
+     private static final int BOOK_5 = 5;
+
+     /** Number of 2 tickets to book. */
+     private static final int BOOK_2 = 2;
+
+     /** Number of 3 tickets to cancel. */
+     private static final int CANCEL_3 = 3;
+
+     /** Number of 5 tickets to cancel. */
+     private static final int CANCEL_5 = 5;
+
+     /** Overbooking attempt of 100 tickets. */
+     private static final int OVERBOOK = 100;
+
 
     @BeforeEach
     void setUp() {
@@ -29,7 +53,8 @@ class MovieBookingSystemTest {
     @Test
     void testBookTicketSuccess() {
         String result = mbs.bookTicket(SHOW_10_AM, BOOK_5);
-        assertEquals(BOOK_5 + " tickets successfully booked for " + SHOW_10_AM, result);
+        assertEquals(BOOK_5 + " tickets successfully booked for "
+        + SHOW_10_AM, result);
     }
 
     @Test
@@ -42,20 +67,23 @@ class MovieBookingSystemTest {
     void testCancelReservationSuccess() {
         mbs.bookTicket(SHOW_10_AM, BOOK_5);
         String result = mbs.cancelReservation(SHOW_10_AM, CANCEL_3);
-        assertEquals(CANCEL_3 + " tickets successfully canceled for " + SHOW_10_AM, result);
+        assertEquals(CANCEL_3 + " tickets successfully canceled for "
+        + SHOW_10_AM, result);
     }
 
     @Test
     void testBookAnotherShowtime() {
         String result = mbs.bookTicket(SHOW_1_PM, BOOK_2);
-        assertEquals(BOOK_2 + " tickets successfully booked for " + SHOW_1_PM, result);
+        assertEquals(BOOK_2 + " tickets successfully"
+            + "booked for " + SHOW_1_PM, result);
     }
 
     @Test
     void testCancelMoreThanBooked() {
         mbs.bookTicket(SHOW_1_PM, BOOK_2);
         String result = mbs.cancelReservation(SHOW_1_PM, CANCEL_5);
-        assertEquals("Invalid operation (Attempt to cancel more tickets than booked).", result);
+        assertEquals("Invalid operation (Attempt to cancel"
+            + "more tickets than booked).", result);
     }
 
     @Test
